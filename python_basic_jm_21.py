df08d5a919eb3dbb8be560af0a385ee0938a5396
jmkim <todaybow@naver.com>
2020-02-04 23:27:22 +0900

    python basic jm 22

--- python_basic_jm_21.py
+++ python_basic_jm_21.py
@@ -36,13 +36,30 @@ cursor.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, usernam
 
 # 데이터 삽입
 cursor.execute("INSERT INTO users VALUES(1, 'KIMJAEMIN', 'todaybow@naver.com', '010-3309-7000', 'www.tigerai.net.cn', ?)", (nowDatetime,))
+cursor.execute("INSERT INTO users(id, username, e_mail, phone, website, regdate) VALUES(?,?,?,?,?,?)", (2, 'ChoiHeeyeon', 'top2road@naver.com','010-9xxx-xxxx', 'www.naver.com', nowDatetime))
+
+# Many 삽입(튜플, 리스트)
+userList = (
+    (3, 'Lee', 'duam@naver.com','010-9xxx-xxxx', 'www.naver.com', nowDatetime),
+    (4, 'Park', 'hanmail@naver.com','010-9xxx-xxxx', 'www.daum.com', nowDatetime),
+    (5, 'Joo', 'cake@naver.com','010-9xxx-xxxx', 'www.hanmail.com', nowDatetime),
+)
+
+cursor.executemany("INSERT INTO users(id, username, e_mail, phone, website, regdate) VALUES (?,?,?,?,?,?)", userList)
+
+# 테이블 데이터 삭제
+# conn.execute("DELETE FROM users")
+print("users db deleted : ", conn.execute("DELETE FROM users").rowcount)
 
 
+# 커밋(Commit) : isolation_level = None 일경우 auto commit
+# 커밋(Commit) : isolation_level = None 이 아닐 경우 conn.commit() 명령으로 종료해야합니다.
 
-'''
-https://online.fastcampus.co.kr/courses/510383/lectures/10074682
-'''
+# 롤백
+# conn.rollback()
 
+# 접속 종료
+conn.close()
 
 
 
